89eb72c6a62986d08ef2e8dae72f48018a8c9ce0
Musaab mohammed <musaab.aljarba9@gmail.com>
2021-09-13 19:52:12 +0300

    Initial Commit

---
+++ SaryApp/SaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaryAppApp.swift
+//  SaryApp
+//
+//  Created by Musab Aljarba on 06/02/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
